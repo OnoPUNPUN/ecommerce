bb734a5aece657bc3585a98cdf7f3838b14d12c8
OnoPUNPUN <punpunonodera754@gmail.com>
2025-08-28 15:31:46 +0600

    Added Firebase core

---
+++ android/app/src/main/kotlin/com/onopunpun/ecommerce/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.onopunpun.ecommerce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:ecommerce/app/app.dart';
+import 'package:flutter/material.dart';
+
+/*
+  !!Things to do before starting making the app:
+    1. Localization
+    2. Theme
+    3. Crashlytics
+    4. Analytics
+*/
+
+void main() {
+  runApp(const CraftyBay());
+}
